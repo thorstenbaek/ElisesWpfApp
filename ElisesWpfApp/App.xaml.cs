2aa785edcc4ebe10f49538782a64bd8ba5b59ae5
Thor Stenbæk <tst@dips.no>
2021-06-11 15:00:55 +0200

    Add project files.

---
+++ ElisesWpfApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ElisesWpfApp.ViewModels;
+using ElisesWpfApp.Views;
+
+namespace ElisesWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel("Elise")
+            };
+
+            MainWindow.Show();
+
+        }
+    }
+}
---
+++ ElisesWpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Commands;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ElisesWpfApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public ICommand SendButtonCommand { get; }
+
+        public MainViewModel(string firstName)
+        {
+            FirstName = firstName;
+
+            SendButtonCommand = new DelegateCommand(DoSend);
+        }
+
+        private void RaisePropertyChanged(string property) 
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void DoSend()
+        {
+            Title = FirstName;
+            RaisePropertyChanged("Title");
+        }
+    }
+}
